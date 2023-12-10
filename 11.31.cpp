6fb017e767b6b1d4f7e1dc01b1d2a5b8670bbafc
LINA-VA04 <152481764+LINA-VA04@users.noreply.github.com>
2023-12-10 20:42:45 +0400

    Create 11.31.cpp

---
+++ 11.31.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string yourUniversity = "Манас";
+    std::string yourMajor = "Программист";
+
+    std::cout << "Привет! Я учусь в университете " << yourUniversity << " на специальности " << yourMajor << "." << std::endl;
+ 
+
+    return 0;
+}
+
