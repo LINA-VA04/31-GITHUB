75dedfc05d5f4463a21146942d75ad24635bd689
LINA-VA04 <152481764+LINA-VA04@users.noreply.github.com>
2023-12-19 13:04:27 +0400

    Create 20.31.cpp

---
+++ 20.31.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{ char symbol= 'd';
+char digit= '9';
+char space ='0';
+char character ='#';
+isalnum( symbol) ? cout << "t\n": cout << "f\n";
+isalpha(symbol) ? cout <<"t\n": cout << "f\n";
+isdigit(digit) ?cout << "t\n": cout << "f\n";
+isspace(space) ? cout << "t\n": cout << "f\n";
+islower(symbol) ? cout << "t\n": cout << "f\n";
+isupper (symbol) ? cout << "t\n": cout << "f\n";
+return 0;} 
+
